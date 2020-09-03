55aab0a0c72c3bd00477f32bebb6575f6b9d8ff9
uluk01 <ulukbakytbekov@gmail.com>
2020-09-03 21:20:17 +0600

    Add footer section

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import render from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Footer from "./components/Footer/Footer";
 
-render(
-    <App />,
+ReactDOM.render(
+    <Footer/>,
   document.getElementById('root')
 );
