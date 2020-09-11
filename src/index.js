eb29767a16e070c5e6491630e75206461a7d4f99
Nursultan G <nomminor@gmail.com>
2020-09-11 23:16:32 +0600

    Fixed error includes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import render from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-render(
+ReactDOM.render(
     <App />,
   document.getElementById('root')
 );
