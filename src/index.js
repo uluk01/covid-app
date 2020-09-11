92412a23fe7a9d7af389b5d7396cc3f534bda3cb
Nurtilek <nurdinov.nurtilek@gmail.com>
2020-09-11 09:33:46 +0600

    Added main font style and removed unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 
 
-ReactDOM.render(
+render(
     <App/>,
   document.getElementById('root')
 );
